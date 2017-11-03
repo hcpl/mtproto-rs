f2bb20f5cdc14c05bcdb6fbfe6c1446acf06b425
hcpl <hcpl.prog@gmail.com>
2017-11-03 22:27:16 +0200

    Use library rustfmt instead of the binary one
    Binary rustfmt can not be present at all, since it's not distributed
    with rustc (previously that was assumed to be true).

--- build.rs
+++ build.rs
@@ -1,32 +1,19 @@
-extern crate env_logger;
-#[macro_use]
-extern crate error_chain;
-#[macro_use]
-extern crate log;
+extern crate rustfmt;
 extern crate tl_codegen;
-
 
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Read, Write};
 use std::path::{Path, PathBuf};
-use std::process::Command;
 
-
-mod error {
-    error_chain! {
-        foreign_links {
-            Io(::std::io::Error);
-            SetLogger(::log::SetLoggerError);
-        }
-    }
-}
+use rustfmt::Input;
+use rustfmt::config::Config;
 
 
 const TL_SCHEMA_DIR:       &'static str = "./tl";
 const TL_SCHEMA_LIST_FILE: &'static str = "./tl/tl-schema-list.txt";
 const RUST_SCHEMA_FILE:    &'static str = "./src/schema.rs";
 
-fn collect_input() -> error::Result<String> {
+fn collect_input() -> io::Result<String> {
     let mut tl_files = BufReader::new(File::open(TL_SCHEMA_LIST_FILE)?).lines().filter_map(|line| {
         match line {
             Ok(ref line) if line.starts_with("//") => None,  // This line is a comment
@@ -36,7 +23,6 @@ fn collect_input() -> error::Result<String> {
     }).collect::<io::Result<Vec<PathBuf>>>()?;
 
     tl_files.sort();
-    debug!("Files detected: {:?}", &tl_files);
     println!("cargo:rerun-if-changed={}", TL_SCHEMA_LIST_FILE);
 
     let mut input = String::new();
@@ -48,23 +34,23 @@ fn collect_input() -> error::Result<String> {
     Ok(input)
 }
 
-fn run() -> error::Result<()> {
-    env_logger::init()?;
-
+fn run() -> io::Result<()> {
     let input = collect_input()?;
     let code = tl_codegen::generate_code_for(&input);
-    debug!("Code size: {} bytes", code.as_str().len());
-
     File::create(RUST_SCHEMA_FILE)?.write_all(code.as_str().as_bytes())?;
-    debug!("Successful write to {}", RUST_SCHEMA_FILE);
 
-    Command::new("rustfmt")
-        .arg("--write-mode")
-        .arg("overwrite")
-        .arg(RUST_SCHEMA_FILE)
-        .status()?;
+    // Make output from `quote!`s human-readable
+    let input = Input::File(PathBuf::from(RUST_SCHEMA_FILE));
+    let config = Config::from_toml_path(Path::new("rustfmt.toml")).unwrap_or_else(|_| Config::default());
 
-    Ok(())
+    if rustfmt::run(input, &config).has_no_errors() {
+        Ok(())
+    } else {
+        let msg = format!("error while running `rustfmt` on {}", RUST_SCHEMA_FILE);
+        Err(io::Error::new(io::ErrorKind::Other, msg))
+    }
 }
 
-quick_main!(run);
+fn main() {
+    run().unwrap()
+}
