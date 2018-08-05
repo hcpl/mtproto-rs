8c6adbe29955acf33287f1d5387c94ebc7395cfa
hcpl <hcpl.prog@gmail.com>
2018-08-05 15:27:01 +0300

    Finish 3rd step of auth and begin 4th

--- examples/auth.rs
+++ examples/auth.rs
@@ -1,4 +1,5 @@
 extern crate byteorder;
+extern crate chrono;
 extern crate dotenv;
 extern crate env_logger;
 #[macro_use]
@@ -16,7 +17,8 @@ extern crate serde_mtproto;
 extern crate tokio;
 
 
-use byteorder::{BigEndian, ByteOrder};
+use byteorder::{BigEndian, ByteOrder, LittleEndian};
+use chrono::Utc;
 use extprim::i128;
 use futures::{Future, Stream};
 use mtproto::I256;
@@ -25,7 +27,9 @@ use mtproto::rpc::session::SessionConnection;
 use mtproto::rpc::connection::ConnectionConfig;
 use mtproto::rpc::encryption::asymm;
 use mtproto::schema;
-use serde_mtproto::Boxed;
+use openssl::{aes, bn, hash, symm};
+use serde_mtproto::{Boxed, MtProtoSized};
+use rand::Rng;
 
 
 mod error {
@@ -50,6 +54,11 @@ mod error {
             ServerDHParamsFail {
                 description("server didn't send DH parameters")
                 display("server didn't send DH parameters")
+            }
+
+            SetClientDHParamsAnswerFail {
+                description("server failed to verify DH parameters")
+                display("server failed to verify DH parameters")
             }
         }
     }
@@ -95,7 +104,7 @@ fn auth(conn_config: ConnectionConfig) -> Box<Future<Item = (), Error = error::E
         .and_then(unpack!(auth_step1(session_conn)))
         .and_then(unpack!(auth_step2(session_conn, response, nonce)))
         .and_then(unpack!(auth_step3(session_conn, response, nonce, server_nonce, new_nonce)))
-        .and_then(unpack!(auth_step4(session_conn, response, nonce, server_nonce, new_nonce)));
+        .and_then(unpack!(auth_step4(session_conn, response, nonce, server_nonce, new_nonce, auth_key, time_offset)));
 
     Box::new(auth_future)
 }
@@ -221,6 +230,8 @@ fn auth_step3(session: SessionConnection,
            i128::i128,
            i128::i128,
            I256,
+           Vec<u8>,
+           i32,
        ), Error = error::Error> + Send>
 {
     info!("Received server DH parameters: {:#?}", server_dh_params);
@@ -232,35 +243,8 @@ fn auth_step3(session: SessionConnection,
             Box::new(futures::future::err(ErrorKind::ServerDHParamsFail.into()))
         },
         schema::Server_DH_Params::server_DH_params_ok(server_dh_params_ok) => {
-            use byteorder::LittleEndian;
-            use openssl::{aes, hash, symm};
-
-            fn little_endian_i128_to_bytes(n: i128::i128) -> [u8; 16] {
-                let mut buf = [0; 16];
-                LittleEndian::write_u64(&mut buf[0..8], n.low64());
-                LittleEndian::write_i64(&mut buf[8..16], n.high64());
-                buf
-            }
-
-            fn little_endian_i256_to_bytes(n: I256) -> [u8; 32] {
-                let mut buf = [0; 32];
-                LittleEndian::write_u64(&mut buf[0..8], n.low128().low64());
-                LittleEndian::write_u64(&mut buf[8..16], n.low128().high64());
-                LittleEndian::write_u64(&mut buf[16..24], n.high128().low64());
-                LittleEndian::write_i64(&mut buf[24..32], n.high128().high64());
-                buf
-            }
-
             let server_nonce_bytes = little_endian_i128_to_bytes(server_nonce);
             let new_nonce_bytes = little_endian_i256_to_bytes(new_nonce);
-
-            fn sha1_from_bytes(bytes: &[&[u8]]) -> error::Result<hash::DigestBytes> {
-                let mut hasher = hash::Hasher::new(hash::MessageDigest::sha1())?;
-                for b in bytes {
-                    hasher.update(b)?;
-                }
-                hasher.finish().map_err(Into::into)
-            }
 
             let hash1 = tryf!(sha1_from_bytes(&[&new_nonce_bytes, &server_nonce_bytes]));
             let hash2 = tryf!(sha1_from_bytes(&[&server_nonce_bytes, &new_nonce_bytes]));
@@ -277,35 +261,50 @@ fn auth_step3(session: SessionConnection,
             // Key is 256-bit => can unwrap safely
             let aes_decrypt_key = aes::AesKey::new_decrypt(&tmp_aes_key).unwrap();
             let mut decrypted_answer = vec![0; server_dh_params_ok.encrypted_answer.len()];
-            aes::aes_ige(server_dh_params_ok.encrypted_answer.as_ref(), decrypted_answer.as_mut_slice(), &aes_decrypt_key, &mut tmp_aes_iv, symm::Mode::Decrypt);
+            aes::aes_ige(server_dh_params_ok.encrypted_answer.as_ref(), decrypted_answer.as_mut_slice(), &aes_decrypt_key, &mut tmp_aes_iv.clone(), symm::Mode::Decrypt);
 
-            // FIXME: use `SHA1_HASH_LENGTH` or something
-            let (answer_server_hash, answer_bytes) = decrypted_answer.split_at(20);
+            const SHA1_HASH_LENGTH: usize = 20;
+            let (answer_server_hash, answer_bytes) = decrypted_answer.split_at(SHA1_HASH_LENGTH);
             let (answer, random_tail) = tryf!(serde_mtproto::from_bytes_reuse::<Boxed<schema::Server_DH_inner_data>>(answer_bytes, &[]));
             let answer_len = answer_bytes.len() - random_tail.len();
             let answer_client_hash = tryf!(sha1_from_bytes(&[&answer_bytes[0..answer_len]]));
             assert_eq!(answer_server_hash, answer_client_hash.as_ref());
 
-            let g = tryf!(openssl::bn::BigNum::from_u32(answer.inner().g as u32));
-            let mut b = tryf!(openssl::bn::BigNum::new());
-            tryf!(b.rand(2048, openssl::bn::MsbOption::ONE, true));
-            let dh_prime = tryf!(openssl::bn::BigNum::from_slice(&*answer.inner().dh_prime));
-            let mut g_b = tryf!(openssl::bn::BigNum::new());
-            let mut ctx = tryf!(openssl::bn::BigNumContext::new());
+            let g = tryf!(bn::BigNum::from_u32(answer.inner().g as u32));
+            let mut b = tryf!(bn::BigNum::new());
+            tryf!(b.rand(2048, bn::MsbOption::ONE, true));
+            let dh_prime = tryf!(bn::BigNum::from_slice(&*answer.inner().dh_prime));
+            let mut g_b = tryf!(bn::BigNum::new());
+            let mut ctx = tryf!(bn::BigNumContext::new());
             tryf!(g_b.mod_exp(&g, &b, &dh_prime, &mut ctx));
 
             let client_dh_inner_data = Boxed::new(schema::Client_DH_Inner_Data {
                 nonce,
                 server_nonce,
-                retry_id: 0,
+                retry_id: 0,  // TODO: actual retry ID
                 g_b: g_b.to_vec().into(),
             });
 
-            let client_dh_inner_data_bytes = tryf!(serde_mtproto::to_bytes(&client_dh_inner_data));
+            let client_dh_inner_data_len = tryf!(client_dh_inner_data.size_hint());
+            let random_tail_len = (16 - ((SHA1_HASH_LENGTH + client_dh_inner_data_len) % 16)) % 16;
+            let mut client_dh_inner_data_to_encrypt =
+                vec![0; SHA1_HASH_LENGTH + client_dh_inner_data_len + random_tail_len];
+
+            {
+                let (client_dh_inner_data_hash, client_dh_inner_data_rest) =
+                    client_dh_inner_data_to_encrypt.split_at_mut(SHA1_HASH_LENGTH);
+                let (client_dh_inner_data_bytes, random_tail) =
+                    client_dh_inner_data_rest.split_at_mut(client_dh_inner_data_len);
+                tryf!(serde_mtproto::to_writer(&mut *client_dh_inner_data_bytes, &client_dh_inner_data));
+                let hash_bytes = tryf!(sha1_from_bytes(&[client_dh_inner_data_bytes]));
+                client_dh_inner_data_hash.copy_from_slice(&*hash_bytes);
+                rand::thread_rng().fill_bytes(random_tail);
+            }
+
             // Key is 256-bit => can unwrap safely
             let aes_encrypt_key = aes::AesKey::new_encrypt(&tmp_aes_key).unwrap();
-            let mut encrypted_data = vec![0; client_dh_inner_data_bytes.len()];
-            aes::aes_ige(&client_dh_inner_data_bytes, encrypted_data.as_mut_slice(), &aes_encrypt_key, &mut tmp_aes_iv, symm::Mode::Encrypt);
+            let mut encrypted_data = vec![0; client_dh_inner_data_to_encrypt.len()];
+            aes::aes_ige(&client_dh_inner_data_to_encrypt, encrypted_data.as_mut_slice(), &aes_encrypt_key, &mut tmp_aes_iv.clone(), symm::Mode::Encrypt);
 
             let set_client_dh_params = schema::rpc::set_client_DH_params {
                 nonce,
@@ -315,8 +314,21 @@ fn auth_step3(session: SessionConnection,
 
             let request = session.request(set_client_dh_params, MessageType::PlainText, MessageType::PlainText);
 
+            let g_a = tryf!(bn::BigNum::from_slice(&*answer.inner().g_a));
+            let mut g_ab = tryf!(bn::BigNum::new());
+            tryf!(g_ab.mod_exp(&g_a, &b, &dh_prime, &mut ctx));
+            let auth_key = g_ab.to_vec();
+
+            // Hopefully server will use 64-bit integers before Year 2038
+            // Problem kicks in
+            let local_timestamp = Utc::now().timestamp() as i32;
+            let time_offset = answer.inner().server_time - local_timestamp;
+
             Box::new(request.map(move |(session_conn, response)| {
-                (session_conn, response, nonce, server_nonce, new_nonce)
+                (
+                    session_conn, response, nonce, server_nonce, new_nonce,
+                    auth_key, time_offset,
+                )
             }).map_err(Into::into))
         },
     }
@@ -326,12 +338,36 @@ fn auth_step4(_session: SessionConnection,
               set_client_dh_params_answer: schema::Set_client_DH_params_answer,
               _nonce: i128::i128,
               _server_nonce: i128::i128,
-              _new_nonce: I256)
+              _new_nonce: I256,
+              auth_key: Vec<u8>,
+              _time_offset: i32)
     -> Box<Future<Item = (), Error = error::Error> + Send>
 {
     info!("Received server DH verification: {:#?}", set_client_dh_params_answer);
 
-    Box::new(futures::future::ok(()))
+    let auth_key_sha1 = tryf!(sha1_from_bytes(&[&auth_key]));
+    let mut auth_key_hash = [0; 8];
+    auth_key_hash.copy_from_slice(&auth_key_sha1[0..8]);
+    let mut auth_key_aux_hash = [0; 8];
+    auth_key_aux_hash.copy_from_slice(&auth_key_sha1[12..20]);
+
+    match set_client_dh_params_answer {
+        schema::Set_client_DH_params_answer::dh_gen_ok(dh_gen_ok) => {
+            info!("DH params verification succeeded: {:?}", dh_gen_ok);
+
+            Box::new(futures::future::ok(()))
+        },
+        schema::Set_client_DH_params_answer::dh_gen_retry(dh_gen_retry) => {
+            info!("DH params verification needs a retry: {:?}", dh_gen_retry);
+
+            unimplemented!();
+        },
+        schema::Set_client_DH_params_answer::dh_gen_fail(dh_gen_fail) => {
+            error!("DH params verification failed: {:?}", dh_gen_fail);
+
+            Box::new(futures::future::err(ErrorKind::SetClientDHParamsAnswerFail.into()))
+        },
+    }
 }
 
 
@@ -349,6 +385,30 @@ fn fetch_app_info() -> error::Result<AppInfo> {
         "this example needs either both `MTPROTO_API_ID` and `MTPROTO_API_HASH` environment \
          variables set, or an AppInfo.toml file with `api_id` and `api_hash` fields in it"
     })
+}
+
+fn little_endian_i128_to_bytes(n: i128::i128) -> [u8; 16] {
+    let mut buf = [0; 16];
+    LittleEndian::write_u64(&mut buf[0..8], n.low64());
+    LittleEndian::write_i64(&mut buf[8..16], n.high64());
+    buf
+}
+
+fn little_endian_i256_to_bytes(n: I256) -> [u8; 32] {
+    let mut buf = [0; 32];
+    LittleEndian::write_u64(&mut buf[0..8], n.low128().low64());
+    LittleEndian::write_u64(&mut buf[8..16], n.low128().high64());
+    LittleEndian::write_u64(&mut buf[16..24], n.high128().low64());
+    LittleEndian::write_i64(&mut buf[24..32], n.high128().high64());
+    buf
+}
+
+fn sha1_from_bytes(bytes: &[&[u8]]) -> error::Result<hash::DigestBytes> {
+    let mut hasher = hash::Hasher::new(hash::MessageDigest::sha1())?;
+    for b in bytes {
+        hasher.update(b)?;
+    }
+    hasher.finish().map_err(Into::into)
 }
 
 
